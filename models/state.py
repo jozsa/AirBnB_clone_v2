4c6af9d69d458c386e0f61ed255f377113885c50
jozsa <allisonjoyweiner@gmail.com>
2019-03-31 22:15:26 +0000

    Clearer documentation, import sqlalchemy and add Base import. Change attributes to sql table columns

--- models/state.py
+++ models/state.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class State(BaseModel):
-    """This is the class for State
-    Attributes:
-        name: input name
+class State(BaseModel, Base):
     """
-    name = ""
+    This is the class for State objects.
+    It is associated with the SQL table 'states'.
+
+    Attributes:
+        name: String, 128 characters
+    """
+    name = Column(String(128),
+                  nullable=False)
+
+    """
+    TODO:
+    Add relationship between state/city (different for FileStorage and DBStorage)
+    Haven't done DBStorage or set env vars so still unsure how to do this.
+    """
