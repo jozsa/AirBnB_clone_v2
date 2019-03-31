1b0dc2c7aa5b8b60fa6cd71ce9594f095db260a2
jozsa <allisonjoyweiner@gmail.com>
2019-03-31 21:30:12 +0000

    Change attributes to SQL table columns, state_id foreign key to states.id

--- models/city.py
+++ models/city.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 """This is the city class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String
 
 
-class City(BaseModel):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
+class City(BaseModel, Base):
     """
-    state_id = ""
-    name = ""
+    This is the class for City. It is linked to the cities table
+
+    Attributes:
+        state_id: String, Foreign Key to State ID associated with city
+        name: String, name of city
+    """
+    __tablename__ = 'cities'
+
+    state_id = Column(String(60),
+                      nullable=False,
+                      ForeignKey(State.id))
+    name = Column(String(128),
+                  nullable=false)
