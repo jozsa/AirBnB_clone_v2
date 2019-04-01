f3e5476ad86561c8443f79b647f5592ef34cfd8b
jozsa <allisonjoyweiner@gmail.com>
2019-04-01 23:35:04 +0000

    Added relationship for db_storage and for file_storage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This is the state class"""
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-
+from sqlalchemy.orm import relationship, backref
 
 class State(BaseModel, Base):
     """
@@ -17,8 +18,13 @@ class State(BaseModel, Base):
     name = Column(String(128),
                   nullable=False)
 
-    """
-    TODO:
-    Add relationship between state/city (different for FileStorage and DBStorage)
-    Haven't done DBStorage or set env vars so still unsure how to do this.
-    """
+    if 'HBNB_TYPE_STORAGE' in os.environ:
+        if os.environ['HBNB_TYPE_STORAGE'] == 'db':
+            cities = relationship('City', backref='state')
+    else:
+        @property
+        def cities(self):
+            """Property getter of list of city instances
+            where state_id equals current State.id"""
+            city_dict = storage.all(City)
+            return [city for city in city_dict.values() if city.state_id == self.id]
