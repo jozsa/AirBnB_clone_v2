02b25730ef74fa044f27e24b0121219342a05e0f
jozsa <allisonjoyweiner@gmail.com>
2019-04-01 22:46:10 +0000

    Change try/except to if/else

--- models/__init__.py
+++ models/__init__.py
@@ -9,14 +9,12 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
-try:
+if "HBNB_TYPE_STORAGE" in os.environ:
     if os.environ['HBNB_TYPE_STORAGE'] == 'db':
         from models.engine.db_storage import DBStorage
         storage = DBStorage()
         storage.reload()
-    else:
-        from models.engine.file_storage import FileStorage
-        storage = FileStorage()
-        storage.reload()
-except KeyError:
-    pass
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
