a80e2bb27b28d3f61cb331ff15f326da33e609d7
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-04-02 03:42:41 +0000

    PEP8 fixes

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
 
+
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
@@ -17,9 +18,9 @@ class BaseModel:
     id = Column(String(60),
                 primary_key=True)
     created_at = Column(DateTime,
-                        default = datetime.utcnow())
+                        default=datetime.utcnow())
     updated_at = Column(DateTime,
-                        default = datetime.utcnow())
+                        default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
