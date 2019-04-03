0e4421cc1861b2e8c96a0b92de4e582d9086a299
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-04-03 23:31:47 +0000

    Added imports

--- models/place.py
+++ models/place.py
@@ -2,7 +2,7 @@
 """This is the place class"""
 import os
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship, backref
 
 
