00e906c3c697592bf7ff0e614824a6cc6ee0c574
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-04-03 05:23:20 +0000

    Change Place class attributes to work with DB storage

--- models/place.py
+++ models/place.py
@@ -1,31 +1,54 @@
 #!/usr/bin/python3
 """This is the place class"""
-from models.base_model import BaseModel
+# import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+# from sqlalchemy.orm import relationship, backref
 
 
-class Place(BaseModel):
-    """This is the class for Place
+class Place(BaseModel, Base):
+    """
+    This is the class for Place objects.
+
     Attributes:
-        city_id: city id
-        user_id: user id
-        name: name input
-        description: string of description
-        number_rooms: number of room in int
-        number_bathrooms: number of bathrooms in int
-        max_guest: maximum guest in int
-        price_by_night:: pice for a staying in int
-        latitude: latitude in flaot
-        longitude: longitude in float
+        city_id: non-Null String from table `cities`, 60 characters
+        user_id: non-Null String from table `users`, 60 characters
+        name: non-Null String, 128 characters
+        description: nullable String, 1024 characters
+        number_rooms: non-Null Integer, default 0
+        number_bathrooms: non-Null Integer, default 0
+        max_guest: non-Null Integer, default 0
+        price_by_night: non-Null Integer, default 0
+        latitude: nullable Float, default 0
+        longitude: nullable Float, default 0
         amenity_ids: list of Amenity ids
     """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+
+    city_id = Column(String(60),
+                     ForeignKey('cities.id'),
+                     nullable=False)
+    user_id = Column(String(60),
+                     ForeignKey('users.id'),
+                     nullable=False)
+    name = Column(String(128),
+                  nullable=False)
+    description = Column(String(1024),
+                         nullable=True)
+    number_rooms = Column(Integer,
+                          nullable=False,
+                          default=0)
+    number_bathrooms = Column(Integer,
+                              nullable=False,
+                              default=0)
+    max_guest = Column(Integer,
+                       nullable=False,
+                       default=0)
+    price_by_night = Column(Integer,
+                            nullable=False,
+                            default=0)
+    latitude = Column(Float,
+                      nullable=True)
+    longitude = Column(Float,
+                       nullable=True)
     amenity_ids = []
