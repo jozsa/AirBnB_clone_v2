b1a99576c91c22b084fb575fee4251687a94604f
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-04-03 05:54:08 +0000

    WIP added `places` relationship with class `Place`

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the city class"""
+import os
 from models.base_model import BaseModel, Base
 from models.state import State
 from sqlalchemy import Column, String, ForeignKey
@@ -10,13 +11,20 @@ class City(BaseModel, Base):
     This is the class for City. It is linked to the cities table
 
     Attributes:
-        state_id: String, Foreign Key to State ID associated with city
-        name: String, name of city
+        state_id: non-Null String, Foreign Key to State ID associated with city
+        name: non-Null String, name of city
     """
     __tablename__ = 'cities'
 
+    # TODO verify this syntax for ForeignKey
+    # https://docs.sqlalchemy.org/en/latest/orm/examples.html#writing-your-own-suites
     state_id = Column(String(60),
                       ForeignKey(State.id),
                       nullable=False)
     name = Column(String(128),
                   nullable=False)
+
+    if 'HBNB_TYPE_STORAGE' in os.environ:
+        if os.environ['HBNB_TYPE_STORAGE'] == 'db':
+            # TODO implement the deletion requirement
+            places = relationship('Place', backref='cities')
