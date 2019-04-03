ee49e6e4648f3c70d7de3e17848c5341478ee9bd
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-04-03 16:37:11 +0000

    Change Review class attributes to work with DB storage

--- models/review.py
+++ models/review.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
 """This is the review class"""
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
 
 
 class Review(BaseModel):
-    """This is the class for Review
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
     """
-    place_id = ""
-    user_id = ""
-    text = ""
+    This is the class for Review objects.
+    It is associated with the SQL table 'reviews'.
+
+    Attributes:
+        place_id: non-Null String from table `places`, 60 characters
+        user_id: non-Null String from table `users`, 60 characters
+        text: non-Null String, 1024 characters
+    """
+    __tablename__ = 'reviews'
+
+    place_id = Column(String(60),
+                      ForeignKey('places.id'),
+                      nullable=False)
+    user_id = Column(String(60),
+                     ForeignKey('users.id'),
+                     nullable=False)
+    text = Column(String(1024),
+                  nullable=False)
