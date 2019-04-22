8f01cdc0f47448d2d8e6e345f021208941a41a01
jozsa <allisonjoyweiner@gmail.com>
2019-04-22 20:34:52 +0000

    Returning a string for location / using Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+from flask import Flask
+"""
+One function: Returns "Hello HBNB!" to
+anyone submitting a GET request to '/'
+on my app
+"""
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Returns Hello HBNB to anyone who submits
+    a GET request to my app"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
