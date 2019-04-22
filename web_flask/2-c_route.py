1ef54593809932ea4549b88864f100558818e059
jozsa <allisonjoyweiner@gmail.com>
2019-04-22 20:35:22 +0000

    Returning a string with a variable for /c/<text> location using Flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+from flask import Flask
+"""
+One function: Returns "Hello HBNB!" to
+anyone submitting a GET request to '/'
+on my app
+"""
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns Hello HBNB to anyone who submits
+    a GET request to my app"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Returns "HBNB" to anyone who submits a
+    GET request to my web server at /hbnb"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Returns 'C' followed by the value of
+    the text variable"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
