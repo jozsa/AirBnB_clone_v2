9f18e0063ce101612ae1149fa2841a5345a74764
jozsa <allisonjoyweiner@gmail.com>
2019-04-24 21:03:13 +0000

    Generating a state list template based on database info

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Starts a flask web app and defines
+rules for routes
+"""
+import json
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """
+    Removes the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    all_states = storage.all("State")
+    return render_template('7-states_list.html', states=all_states)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
